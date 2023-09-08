b52f038d971a7a726fc8aecfeac1d8b725862dab
mjkim <mjkim@xionits.com>
2023-09-09 04:11:48 +0900

    deploy 화면 UI UX 페이지 1. deploy.html  (1) deploy action 초기화 버튼   - package : deployPCK_resetBtn   - computer group : deployGRP_resetBtn  (2) enter parameter display 처리   - 기존 html style 속성 부여한 것을 css로 부여하는 것으로 변경 (none 클래스 / blocked 클래스)  (3) 중복 아이디 변경   - colVal3 -> 동일   - colval3 -> colVal3_param  (4) deploy status css 변경   - p-1 패딩값 부여  (5) action log 확대 버튼   - 버튼을 통해 log 영역을 확대하기 위해 class 및 id 부여    > (class) actionLog_Area : action log 영역    > (class) action_body : card-body 영역    > (id) expand_actionLog : log 확대 버튼  (6) script 추가   - action log 확장 아이콘 이벤트 리스너
    2. deploy_table.js
     (1) ready 호출 내부 코드 수정
      - 기존 deploy_table.js 파일에서 작성된 코드 수정
      - 각 기능에 대해 주석 추가
      - row 클릭 이벤트 발생시 결과값이 들어가는 div 초기화 추가
    
    3. app.min.css
     (1) parameters-scroll 높이값 수정
       - 450px -> 510px
     (2) status-item 보더 컬러 변경
       - border: 1px solid rgba(255,255,255, 1);
     (3) status-sequence 배경 및 폰트 컬러 추가
      - background-color: rgba(255,255,255,.75);
      - color: #000;
     (4) action log 확장 css 추가
      - .expanded {
            position: fixed;
            top: 0;
            left: 0;
            right: 0;
            bottom: 0;
            z-index: 1120;
            border-radius: 0;
            margin: 0 !important;
            padding: 0.3125rem !important;
            background: rgba(29, 40, 53, .95)
        }
      - .action_body {
            height: 230px;
       }

--- static/assets/js/demo/deploy_table.js
+++ static/assets/js/demo/deploy_table.js
@@ -131,8 +131,241 @@ var deploy_computerGroup = function () {
 
 $(document).ready(function () {
 
+    var $clickedRow = ''
+    var resultP = document.getElementById('outputP'); // 왼쪽 package 목록에서 선택된 값이 deploy action에 package 박스에 들어감.
+    var resultC = document.getElementById('outputC'); // 왼쪽 computer group 목록에서 선택된 값이 deploy action에 computer group 박스에 들어감.
+
+    // packageDataTable 테이블의 tbody tr 클릭 이벤트시 호출되는 초기화 함수
+    function resetDivClasses() {
+        var divElement4 = document.getElementById("colVal4");
+        var divElement3 = document.getElementById("colVal3");
+
+        // Set divElement4 to its original state
+        divElement4.classList.remove("col-xl-4");
+        divElement4.classList.add("col-xl-8");
+
+        // Set divElement3 to its original state (hidden)
+        divElement3.style.display = "none";
+    };
+
+    // packageDataTable 테이블의 tbody tr 클릭 이벤트시 호출되는 클래스 부여 함수
+    function toggleDivClasses() {
+        var divElement4 = document.getElementById("colVal4");
+        var divElement3 = document.getElementById("colVal3");
+
+        if (divElement4.classList.contains("col-xl-8")) {
+            divElement4.classList.remove("col-xl-8");
+            divElement4.classList.add("col-xl-4");
+            divElement3.style.display = "block";
+        } else {
+            divElement4.classList.remove("col-xl-4");
+            divElement4.classList.add("col-xl-8");
+            divElement3.style.display = "none";
+        };
+    };
+
+    // packageDataTable 테이블의 tbody tr 이벤트 리스너
+    $("#packageDataTable").on('click', 'tbody tr', function () {
+        $clickedRow = $(this);
+        var isClicked = $clickedRow.hasClass('clicked');
+
+        // 원래 클릭했던 곳의 색을 없애기 위해 다른 행들의 클래스를 제거합니다.
+        $('#packageDataTable tbody tr').not($clickedRow).removeClass('clicked').css('background-color', '');
+        $clickedRow.addClass('clicked');
+
+        var tdValue = $clickedRow.find('td:eq(0)').text(); // 클릭한 행의 첫 번째 셀(td)에서 텍스트를 가져옵니다.
+
+        var parentElement = document.getElementById("colVal3_param");
+        var parentElement1 = document.getElementById("pack"); // deploy action 에 있는 pack
+
+        if (isClicked) {
+            // colVal3_param 안에 있는 div 태그 모두 삭제
+            while (parentElement.firstChild) {
+                parentElement.removeChild(parentElement.firstChild);
+            }
+            // pack 안에 있는 div 태그 모두 삭제
+            while (parentElement1.firstChild) {
+                parentElement1.removeChild(parentElement1.firstChild);
+            }
+            // 클릭한 행이 이미 선택된 상태인 경우 선택 해제
+            $clickedRow.removeClass('clicked');
+            $clickedRow.css('background-color', '');
+            resultP.innerHTML = resultP.innerHTML.replace(tdValue + '<br>', '');
+            $('#packageDataTable tbody tr').css('pointer-events', 'auto');
+            resetDivClasses();
+        } else {
+            // colVal3_param 안에 있는 div 태그 모두 삭제
+            while (parentElement.firstChild) {
+                parentElement.removeChild(parentElement.firstChild);
+            }
+            // pack 안에 있는 div 태그 모두 삭제
+            while (parentElement1.firstChild) {
+                parentElement1.removeChild(parentElement1.firstChild);
+            }
+
+            $clickedRow.addClass('clicked');
+            resultP.innerHTML = tdValue + '<br>';
+            $clickedRow.css('background-color', 'orange');
+            var id = tdValue;
+
+          // Ajax 요청을 보냅니다.
+            $.ajax({
+                type: 'POST', // HTTP 요청 방식을 선택합니다.
+                url: 'packCheck/', // 데이터를 처리할 서버 엔드포인트 URL을 입력합니다.
+                data: { 'id': id }, // 서버로 보낼 데이터를 설정합니다.
+                success: function(response) {
+                  // 성공적인 응답을 받았을 때 수행할 동작을 정의합니다.
+                    console.log('성공:', response);
+                    if (typeof response.a === 'number' && response.a > 0) {
+                        param(response.a);
+                        toggleDivClasses();
+                    } else {
+                        // colVal3_param 안에 있는 div 태그 모두 삭제
+                        while (parentElement.firstChild) {
+                            parentElement.removeChild(parentElement.firstChild);
+                        }
+                        // pack 안에 있는 div 태그 모두 삭제
+                        while (parentElement1.firstChild) {
+                            parentElement1.removeChild(parentElement1.firstChild);
+                        }
+                        return
+                    }
+                },
+                cache: true, // AJAX 호출 결과를 캐시하도록 브라우저에 지시
+                error: function(xhr, status, error) {
+                  // 오류가 발생했을 때 수행할 동작을 정의합니다.
+                    console.error('오류:', status, error);
+                },
+            });
+        }
+    });
+
+    // package에 필요한 파라미터 div 생성
+    function param(count){
+        var parentElement = document.getElementById("colVal3_param"); // input 박스를 추가할 부모 요소를 가져옴
+        var parentElement1 = document.getElementById("pack"); // input 박스를 추가할 부모 요소를 가져옴
+
+        for (var i = 1; i <= count; i++) {
+            // div 태그 생성
+            var divElement = document.createElement("div");
+            divElement.textContent = "parameter " + i;
+            // input 요소 생성
+            var inputElement = document.createElement("input");
+            var inputHidden = document.createElement("input");
+            inputElement.type = "text";
+            inputHidden.type = "hidden";
+            inputElement.name = "param" + i;
+            inputHidden.name = "hidden" + i;
+            inputElement.classList.add("form-control");
+            // div 태그에 input 요소 추가
+            divElement.appendChild(inputElement);
+            // div 태그를 부모 요소에 추가
+            parentElement.appendChild(divElement);
+            parentElement1.appendChild(inputHidden);
+            // 줄 바꿈
+            var brElement = document.createElement("br");
+            parentElement.appendChild(brElement);
+        }
+    };
+
+    // computerGroupDataTable 테이블의 tbody tr 이벤트 리스너
+    $("#computerGroupDataTable").on('click', 'tbody tr', function () {
+        var $clickedRow = $(this);
+        var isClicked = $clickedRow.hasClass('clicked');
+        var tdValue = $clickedRow.find('td:eq(0)').text();
+
+        // 클릭한 행의 상태에 따라 동작을 처리합니다.
+        if (isClicked) {
+            // 클릭한 행이 이미 선택된 상태인 경우 선택 해제
+            $clickedRow.removeClass('clicked');
+            $clickedRow.css('background-color', '');
+            resultC.innerHTML = resultC.innerHTML.replace(tdValue + '<br>', '');
+            $('tbody tr').removeClass('disabled');
+        } else {
+            $('tbody tr').not($clickedRow).addClass('disabled');
+            $clickedRow.addClass('clicked');
+            resultC.innerHTML += tdValue + '<br>';
+            $clickedRow.css('background-color', 'orange');
+        }
+    });
+
+    //outputP 내부 콘텐츠를 재설정하는 기능
+    $("#deployPCK_resetBtn").click(function() {
+        $("#outputP").empty();
+         $("#packageDataTable tbody tr").removeClass('clicked').css('background-color', '');
+    });
+
+    //outputC 내부 콘텐츠를 재설정하는 기능
+    $("#deployGRP_resetBtn").click(function() {
+    $("#outputC").empty();
+    $("#computerGroupDataTable tbody tr").removeClass('clicked').css('background-color', '');
+    });
+
+
+    // deploy action 버튼 이벤트 리스너
+    var outputCValue = document.getElementById('outputC').textContent;
+    document.getElementById('outputCValue').value = outputCValue;
+
+    var errorMsgDiv = document.getElementById('errorMsg');
+    var outputP = document.getElementById('cb1');
+    var outputC = document.getElementById('cb2');
+    var inputs = document.querySelectorAll('.outputP, .outputC');
+    var parentElement1 = document.getElementById("pack");
+
+    inputs.forEach(function (input) {
+        input.addEventListener('input', function () {
+            errorMsgDiv.textContent = '';
+        });
+    });
+
+    // deploy action 버튼 이벤트 리스너
+    document.getElementById('action_btn').addEventListener('click', function (event) {
+        var outputPValue = document.getElementById('outputP').textContent;
+        var outputCValue = document.getElementById('outputC').textContent;
+        var inputElements = document.querySelectorAll("#colVal3_param input");
+        var parentElement = document.getElementById("colVal3_param");
+        console.log(parentElement + '1');
+        inputs.forEach(function (input) {
+            if (input.value === '') {
+                input.classList.add('error-border');
+                event.preventDefault();
+            }
+        });
+
+        // 에러 메시지 출력
+        if (outputPValue === '' || outputCValue === '') {
+            event.preventDefault();
+            errorMsgDiv.textContent = 'package 또는 computer group을 선택해주세요.';
+
+        }else {
+            // 두 개의 값이 모두 있는 경우 에러 메시지 제거
+            errorMsgDiv.innerHTML = '<br/><br/>';
+            document.getElementById('outputPValue').value = outputPValue;
+            document.getElementById('outputCValue').value = outputCValue;
+        }
+
+        inputElements.forEach(function(inputElement, index) {
+            var hiddenInput = document.querySelector('input[name="hidden' +(index+1)+'"]');
+            var paramInput = document.querySelector('input[name="param' +(index+1)+'"]');
+            if (hiddenInput.length > 1) {
+                hiddenInput.value = inputElement.value;
+                paramInput.classList.remove("error")
+                alert(inputElement.value)
+            }else {
+                event.preventDefault();
+                paramInput.classList.add("error")
+                errorMsgDiv.textContent = '파라미터 값을 입력해 주세요.';
+            }
+
+        });
+            if (confirm("배포하시겠습니까?") == true){
+           //true는 확인버튼을 눌렀을 때 코드 작성
+            }else{
+                event.preventDefault();
+                return;
+            }
+    });
     deploy_package();
     deploy_computerGroup();
-
 });
 
